4c48b3cfbf6499cce5d65eb949ccc7112ba8d13f
artem.meniak@gmail.com <user@address.com>
2021-12-30 01:19:14 +0200

    3. New Customer types

---
+++ src/main/java/PersonCustomer.java
@@ -0,0 +1,5 @@
+public class PersonCustomer extends Customer{
+    public PersonCustomer (String name, String surname, String email, CustomerType customerType, Account account) {
+        super(name, surname, email, customerType, account);
+    }
+}
