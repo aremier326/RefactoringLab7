9ab9f1affb24e175196325c26c4973747185952b
artem.meniak@gmail.com <user@address.com>
2021-12-30 01:58:30 +0200

    8. AccountMoney class added

---
+++ src/main/java/AccountMoney.java
@@ -0,0 +1,14 @@
+public class AccountMoney {
+    private double money;
+
+    public AccountMoney() {
+        super();
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+    public double getMoney() {
+        return money;
+    }
+}
